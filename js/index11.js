132199fb157bf6aa9a2127b696849c2a6537bcb0
ExLuZiVe53 <imstudent27@gmail.com>
2024-12-12 21:33:40 +0100

    destructuring objects, arrays and functions

--- js/index11.js
+++ js/index11.js
@@ -1,12 +1,43 @@
 //! Деструктуризація(destructuring assignment) - за допомогою даної функції можливо в об'єкті створити зміну та присвоїти їй значення в одній стрічці
 
-const userProfile = {
-  name: "Billy",
-  commentsQty: 50,
-  hasSignedAgreement: false,
+//* Деструктуризація об'єктів
+
+// const userProfile = {
+//   nameMe: "Billy",
+//   commentsQty: 50,
+//   hasSignedAgreement: false,
+// };
+
+// const { nameMe, commentsQty, hasSignedAgreement } = userProfile;
+
+// console.log(nameMe);
+// console.log(hasSignedAgreement);
+
+//* Деструктуризація масивів
+
+// const fruits = ["Orange", "Kiwi"];
+
+// // Деструктуризація першого та другого елемента в масиві з присвоюванням імені
+
+// const [fruitOne, fruitTwo] = fruits;
+
+// console.log(fruitOne);
+// console.log(fruitTwo);
+
+//* Деструктуризація у функціях
+
+const userProfile1 = {
+  myName: "Charley",
+  myComments: 25,
+  myGender: "male",
 };
 
-const { name, commentsQty, hasSignedAgreement } = userProfile;
+const userInfo = ({ myName, myComments }) => {
+  if (!myComments) {
+    return `User ${myName} has no comments`;
+  } else {
+    return `User ${myName} has ${myComments} comments`;
+  }
+};
 
-console.log(name);
-console.log(hasSignedAgreement);
+console.log(userInfo(userProfile1)); // User Charley has 25 comments
