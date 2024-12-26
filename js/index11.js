ecb6c5009ebaa5b2d72f670be633b23a9eb92b9a
ExLuZiVe53 <imstudent27@gmail.com>
2024-12-26 20:07:56 +0100

    instruction switch and expamples for him

--- js/index11.js
+++ js/index11.js
@@ -173,3 +173,19 @@
 //   default:
 //   // Дія якщо жоден з кейсів не привівся до "true"
 // }
+
+// const month = 12;
+
+// switch (month) {
+//   case 12:
+//     console.log("December");
+//     break;
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("February");
+//     break;
+//   default:
+//     console.log("This is not a winter month");
+// }
