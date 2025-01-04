41b1026fd85b6cc272a5b02611309f505638d797
ExLuZiVe53 <imstudent27@gmail.com>
2025-01-04 22:47:41 +0100

    added examples with modules

---
+++ js/index13.js
@@ -0,0 +1,13 @@
+//! Модулі(Modules)
+
+//* Модулі дозволяють структурувати код
+
+/* -- app
+    -- src
+        index.js;
+        -- modules
+            calc.js
+            dataRequests.js
+            user.js
+    index.html
+ */
