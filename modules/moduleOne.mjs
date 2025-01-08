04e25753878dcae4632e7cfd76f3f3083940281c
ExLuZiVe53 <imstudent27@gmail.com>
2025-01-08 17:12:15 +0100

    added folder modules and rules from them

---
+++ modules/moduleOne.mjs
@@ -0,0 +1,25 @@
+//* Example 1(export default)
+// ============================================================
+
+// const sum = (x, y) => x + y;
+
+// export default sum;
+// ============================================================
+
+//* Example 2(export named)
+
+// const one = 1;
+// const two = "two";
+
+// export { one, two };
+
+// ===========================================================
+
+//* Example 3(export named and rename from import)
+
+// const three = 3;
+// const four = "four";
+
+// export { three, four };
+
+// ===========================================================
---
+++ modules/moduleTwo.mjs
@@ -0,0 +1,27 @@
+//* Example 1(import default)
+// ==================================================
+
+// import sumNumbers from "./moduleOne.mjs";
+
+// const res = sumNumbers(20, 6);
+// console.log(res);
+
+// ==================================================
+
+//* Example 2(import named)
+
+// import { one, two } from "./moduleOne.mjs";
+
+// console.log(one);
+// console.log(two);
+
+// ==================================================
+
+//* Example 3(import named and rename him)
+
+// import { three as threeRenamed, four as fourRenamed } from "./moduleOne.mjs";
+
+// console.log(threeRenamed);
+// console.log(fourRenamed);
+
+// ==================================================
