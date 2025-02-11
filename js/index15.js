81baaf742b12780a0f7a07ec45f8341e271265cd
ExLuZiVe53 <imstudent27@gmail.com>
2025-02-11 20:39:45 +0100

    example with Promise and method fetch

--- js/index15.js
+++ js/index15.js
@@ -12,28 +12,35 @@
 
 //* Створення promise
 
-const myPromise = new Promise((resolve, reject) => {
-  /**
-   * Виконання асинхронних операцій
-   *
-   * Всередині ціє функії потрібно в результаті викликати одну із функцій resolve або reject
-   */
-});
+// const myPromise = new Promise((resolve, reject) => {
+//   /**
+//    * Виконання асинхронних операцій
+//    *
+//    * Всередині ціє функії потрібно в результаті викликати одну із функцій resolve або reject
+//    */
+// });
 
 //? Отримання результату promise
 
-myPromise
-  .then((value) => {
-    /**
-     * Дії у випадку завершення успішного виконання Promise
-     * Значення value - це значення передане у виклику функції resolve всередині Promise
-     */
-    console.log(value);
-  })
-  .catch((error) => {
-    /**
-     * Дії у випадку відхилення Promise
-     * Значення error - це значення передане виклику функції reject всередині Promise
-     */
-    console.log(error);
-  });
+// myPromise
+//   .then((value) => {
+//     /**
+//      * Дії у випадку завершення успішного виконання Promise
+//      * Значення value - це значення передане у виклику функції resolve всередині Promise
+//      */
+//     console.log(value);
+//   })
+//   .catch((error) => {
+//     /**
+//      * Дії у випадку відхилення Promise
+//      * Значення error - це значення передане виклику функції reject всередині Promise
+//      */
+//     console.log(error);
+//   });
+
+//! Використання даних за допомогою fetch API
+
+fetch("https://jsonplaceholder.typicode.com/todos/")
+  .then((response) => response.json())
+  .then((json) => console.log(json))
+  .catch((error) => console.error(error));
